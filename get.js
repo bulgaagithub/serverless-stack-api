224c6beb934c0e078da844e90eb96a08e84c6582
Erdenebulgan <erdenebulgandeveloper@gmail.com>
2021-02-22 13:19:46 +0800

    First commit

---
+++ get.js
@@ -0,0 +1,20 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableName,
+    key: {
+      userid: "123",
+      noteid: event.pathParameters.id,
+    },
+  };
+
+  const result = await dynamoDb.get(params);
+  if (!result.Item) {
+    throw new Error("Item not found.");
+  }
+
+  // Return the retrieved item
+  return result.Item;
+});
---
+++ libs/handler-lib.js
@@ -0,0 +1,22 @@
+export default function handler(lambda) {
+  return async function (event, context) {
+    let body, statusCode;
+
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      // Print out the full error
+      console.log(e);
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+    };
+  };
+}
